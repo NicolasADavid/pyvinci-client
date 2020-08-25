b208ad61a9b1908dc6655a413f3b90ff7a57f52e
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-08-25 14:16:10 -0400

    ProjectName - CreateProject + ProjectsList

--- pyvinci-client/src/ui/components/ProjectsList/ProjectsListProject/ProjectsListProject.js
+++ pyvinci-client/src/ui/components/ProjectsList/ProjectsListProject/ProjectsListProject.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './ProjectsListProject.module.css'
 
-import styles from './ProjectsListProject.css'
 
 export default function ProjectsListProject({project}) {
     return (
         <div className={styles.container}>
             <Link to={"/projects/"+project.id}>
-                {project.id}
+                {project.name}
             </Link>
         </div>
     )
--- pyvinci-client/src/ui/views/CreateProject/CreateProject.js
+++ pyvinci-client/src/ui/views/CreateProject/CreateProject.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import styles from './CreateProject.css'
+import styles from './CreateProject.module.css'
 import ApiService from '../../../services/ApiService';
 
 import { Redirect } from 'react-router-dom';
@@ -8,15 +8,15 @@ import { Redirect } from 'react-router-dom';
 //TODO: Show error details
 export function CreateProject() {
 
-    const [projectName] = useState("");
+    const [projectName, setProjectName] = useState("");
     const [success, setSuccess] = useState(false)
     // const [waiting, setWaiting] = useState(false);
     // const [error, setError] = useState(false);
     // const [errorDetails, setErrorDetails] = useState("")
 
-    // function handleProjectNameChange(event) {
-    //     setProjectName(event.target.value)
-    // }
+    function handleProjectNameChange(event) {
+        setProjectName(event.target.value)
+    }
 
     function handleSubmit(event) {
         ApiService.createProject(projectName)
@@ -32,10 +32,10 @@ export function CreateProject() {
             {success ? <Redirect to='/home' /> : null}
 
             <form onSubmit={handleSubmit}>
-                {/* <label>
+                <label>
                     Project Name:
-                    <textarea value={projectName} onChange={handleProjectNameChange} />
-                </label> */}
+                    <input value={projectName} onChange={handleProjectNameChange} />
+                </label>
                 <input type="submit" value="Create project" />
             </form>
         </div>
